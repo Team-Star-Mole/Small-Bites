5b7857cf9c1577ed9e537dc550e6ed832a56fad4
AndyB909 <mrdawgy12@gmail.com>
2022-06-11 19:27:30 -0700

    rendered menu and bodegas

--- client/component/Test.js
+++ client/component/Test.js
@@ -3,15 +3,30 @@ import React, { useState, useEffect } from 'react';
 
 const Test = () => {
   const [name, setName] = useState([]); // sets state and renders
+  const [menu, setMenu] = useState([]);
   const [number, setNumber] = useState(0)
 
 
   useEffect(() => { // serves as componentDidMount and ...?
-    fetch('/check')
+    fetch('/bodega-names')
     .then((response) => response.json())
     .then(response => setName(response))
     .catch(err => (`Error: ${err}`))
-  })
+  }, [])
+
+  useEffect(() => { // serves as componentDidMount and ...?
+    fetch('/bodega-menu')
+    .then((response) => response.json())
+    .then(response => setMenu(response))
+    .catch(err => (`Error: ${err}`))
+  }, [])
+
+  // const getMenu = () => {
+  //   fetch('/bodega-menu')
+  //   .then((response) => response.json())
+  //   .then(response => setMenu(response))
+  //   .catch(err => (`Error: ${err}`))
+  // }
 
   // console.log("state", name)
   // state = [
@@ -24,21 +39,41 @@ const Test = () => {
   }
   
   let sum = 0
-  function getSum() {
-    name.forEach(el => {
-      sum += Number(el.price)
-      console.log(sum);
-    })
-    return sum;
-  }
+  // function getSum() {
+  //   name.forEach(el => {
+  //     sum += Number(el.price)
+  //     console.log(sum);
+  //   })
+  //   return sum;
+  // }
 
   return (
     <div>Testing react
       <button onClick={handleClick}>Click {number}</button>
 
-          <p>
-            ${getSum()} 
-          </p>
+          <h1>
+            {name.map(el => {
+              return (
+                <h1>{el.bodega_name}</h1>
+
+              )
+            })}
+            {/* ${getSum()}  */}
+          </h1>
+
+          <h2>
+            {menu.map(el => {
+              return(
+                <>
+                  <h3>{el.item_description}</h3>
+                  <h3>${el.price}</h3>
+                  <h3>{el.bodega_id}</h3>
+              </>
+              )
+            })}
+          </h2>
+
+
          
      </div>
   )
--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -7,6 +7,16 @@ router.get('/', getCustomers.getBackendData, (req, res) => {
     res.status(200).json(res.locals.customer);
 })
 
+router.get('/bodega-names', getCustomers.getBodegaNames, (req, res) => {
+    res.status(200).json(res.locals.customer);
+})
+
+router.get('/bodega-menu', getCustomers.getBodegaMenu, (req, res) => {
+    res.status(200).json(res.locals.customer);
+})
+
+
+
 // router.get('/restNames', getC)
 
 module.exports = router;
